4eb756541f1b4a3e8fa5db72beaca0968a4ec581
100seo <deepgreen7362@gmail.com>
2022-05-19 03:41:28 +0900

    Solve BOJ_ 2739ver.2.swift
    Other Solution

--- DSA/BOJ_2739.swift
+++ DSA/BOJ_2739.swift
@@ -3,12 +3,16 @@ import Foundation
 let a = readLine()!
 let b = [1,2,3,4,5,6,7,8,9]
 
-print(Int(a)!,"*",b[0],"=",(Int(a)! * b[0]))
-print(Int(a)!,"*",b[1],"=",(Int(a)! * b[1]))
-print(Int(a)!,"*",b[2],"=",(Int(a)! * b[2]))
-print(Int(a)!,"*",b[3],"=",(Int(a)! * b[3]))
-print(Int(a)!,"*",b[4],"=",(Int(a)! * b[4]))
-print(Int(a)!,"*",b[5],"=",(Int(a)! * b[5]))
-print(Int(a)!,"*",b[6],"=",(Int(a)! * b[6]))
-print(Int(a)!,"*",b[7],"=",(Int(a)! * b[7]))
-print(Int(a)!,"*",b[8],"=",(Int(a)! * b[8]))
+for item in b{
+    print("\(Int(a)!) * \(item) = \(Int(a)! * item)")
+}
+
+//print(Int(a)!,"*",b[0],"=",(Int(a)! * b[0]))
+//print(Int(a)!,"*",b[1],"=",(Int(a)! * b[1]))
+//print(Int(a)!,"*",b[2],"=",(Int(a)! * b[2]))
+//print(Int(a)!,"*",b[3],"=",(Int(a)! * b[3]))
+//print(Int(a)!,"*",b[4],"=",(Int(a)! * b[4]))
+//print(Int(a)!,"*",b[5],"=",(Int(a)! * b[5]))
+//print(Int(a)!,"*",b[6],"=",(Int(a)! * b[6]))
+//print(Int(a)!,"*",b[7],"=",(Int(a)! * b[7]))
+//print(Int(a)!,"*",b[8],"=",(Int(a)! * b[8]))
